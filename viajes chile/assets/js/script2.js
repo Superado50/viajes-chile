fccd599808f4d1b8a207250a3bb2e27783f098fc
Hernan Vidal <hvidalparra@gmail.com>
2022-04-23 18:22:07 -0400

    First Commit viajes chile

---
+++ viajes chile/assets/js/script2.js
@@ -0,0 +1,37 @@
+
+  $(document).ready(function(){
+  $("#boton-en").click(function(){
+    alert("Su mensaje fue enviado satisfactoriamente!");
+  });
+  
+  $(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+
+  
+  /*$(".par2").dblclick(function(){
+    
+      document.getElementById("t2").style.color = "red";
+  });
+
+  $(".t3").click(function(){
+    $("#pa1").toggle("slow")
+      });
+  $(".t4").click(function(){
+    $("#pa2").toggle("slow")
+      });
+  $(".t5").click(function(){
+    $("#pa3").toggle("slow")
+      });*/
+  });
+
+  
+
+
+
+
+
